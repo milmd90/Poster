82847e5a9e708a1aabda1f5836c016ece935fe1b
Matthew Miller <mattdeanmiller@gmail.com>
2017-09-09 01:22:38 -0500

    converting to react

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default App;
