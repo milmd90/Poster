9532fbe87814b19783242c2a3d593907db1e9e51
Matthew Miller <mattdeanmiller@gmail.com>
2017-09-12 00:19:10 -0500

    Change comment

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -39,7 +39,7 @@ function Init() {
     Camera = {x:0, y:0, z:1};
 }
 
-// UpdateRender renders whats in Squares and then updates the canvas
+// UpdateRender renders the design and then updates the canvas
 function UpdateRender() {
     // Set background
     BackContextHandle.fillRect(0, 0, CanvasWidth, CanvasHeight);
